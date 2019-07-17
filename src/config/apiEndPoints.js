44be591eb37fc674f7c9ccd77da3a98220a26265
Rafael Augusto Galvan Fuentes <rgalvan@tiempodevelopment.com>
2019-07-17 11:31:13 -0500

    Made a change on the configs to deploy to heoroku

--- src/config/apiEndPoints.js
+++ src/config/apiEndPoints.js
@@ -1,10 +1,15 @@
 class ApiEndPoints {
 
   createApiUrl(endPoint) {
-    let apiUrl = 'http://localhost:';
+    let apiHost = 'http://localhost:';
     let apiPort = '30001';
+    let prodUrl = 'https://transportes-cams-sanchez.herokuapp.com';
+    let apiUrl = apiHost + apiPort;
 
-    return apiUrl + apiPort + endPoint;
+    if(process.env.PORT) {
+      apiUrl = prodUrl;
+    }
+    return  apiUrl + endPoint;
   }
 
   loginUser() {

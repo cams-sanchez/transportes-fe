246e978e8122a2922bad5474358182e9e54c0831
Rafael Galvan <rafael.augusto.galvan@gmail.com>
2020-03-15 02:30:25 -0600

    Fixed the routes for login, the correct enpoints

--- src/config/UserEndPoints.js
+++ src/config/UserEndPoints.js
@@ -3,13 +3,13 @@ import EndPointFactory from './EndPointFactory';
 class UserEndPoints extends EndPointFactory{
 
   loginUser() {
-    let urlApi = this.createApiUrl('/api/loginUser');
+    let urlApi = this.createApiUrl('/api/users/login');
     console.log("API CALL ", urlApi);
     return urlApi;
   }
 
   userInfoFromToken() {
-    let urlApi = this.createApiUrl('/api/infoFromToken');
+    let urlApi = this.createApiUrl('/api/users/infoFromToken');
     return urlApi;
   }
 }

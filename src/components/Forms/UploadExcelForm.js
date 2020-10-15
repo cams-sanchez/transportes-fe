4668966588a4feede4904724574b63539206a5cd
Rafael Galvan <rafael.augusto.galvan@gmail.com>
2020-10-14 19:25:39 -0500

    Added a dropdown a few changes on the models

--- src/components/Forms/UploadExcelForm.js
+++ src/components/Forms/UploadExcelForm.js
@@ -1,70 +1,120 @@
-import React, {Component} from 'react';
-import {Button, Col, Form, FormGroup, Input, Label, Row} from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Button,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  FormText,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import TirosHelper from "../../helpers/TirosHelper";
 
 class UploadExcelForm extends Component {
+  state = {
+    excelFile: "",
+    cleanName: "",
+    dropdownOpen: false,
+  };
 
-    state = {
-        excelFile: '',
-        cleanName: '',
-    };
+  helper = new TirosHelper();
 
-    helper = new TirosHelper();
+  handleChange = (event) => {
+    const propName = event.target.name;
+    let propValue = event.target.value;
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.files[0],
-            'cleanName': event.target.value
-        });
-    };
-
-    handleSubmit = async (event) => {
-        event.preventDefault();
-
-        let formData = new FormData();
-        formData.append('excelFile', this.state.excelFile);
-
-        //reseting form values
-        this.setState({
-            cleanName: '',
-        });
-
-        if (await this.helper.postUploadExcel(formData) === true) {
-            alert("Se subio correctamente");
-        } else if (this.helper.is401Redirect === true) {
-            this.props.history.push('/login');
-        } else {
-            alert("No se pudo subir el archivo");
-        }
-    };
-
-    render() {
-        return (
-            <Form onSubmit={this.handleSubmit} className="wholeWidth" encType="multipart/form-data">
-                <Row form>
-                    <Col>
-                        <FormGroup>
-                            <Label for="Nombre">Archivo Excel</Label>
-                            <Input value={this.state.cleanName} type="file" name="excelFile" placeholder="Excel File"
-                                   onChange={this.handleChange}/>
-                        </FormGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Button
-                            type="submit"
-                            size="lg"
-                            className="bg-gradient-theme-left border-0 centerButton"
-                            block
-                            onClick={this.handleSubmit}>
-                            Subir Excel
-                        </Button>
-                    </Col>
-                </Row>
-            </Form>
-        );
+    if (propName === "excelFile") {
+      propValue = event.target.files[0];
     }
+
+    this.setState({
+      propName: propValue,
+      cleanName: propValue,
+    });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let formData = new FormData();
+    formData.append("excelFile", this.state.excelFile);
+    formData.append("zonaRepublica", this.state.zonaRepublica);
+
+    //reseting form values
+    this.setState({
+      cleanName: "",
+    });
+
+    if ((await this.helper.postUploadExcel(formData)) === true) {
+      alert("Se subio correctamente");
+    } else if (this.helper.is401Redirect === true) {
+      this.props.history.push("/login");
+    } else {
+      alert("No se pudo subir el archivo");
+    }
+  };
+
+  toggle = () => {
+    this.setState({ dropdownOpen: this.state.dropdownOpen ? false : true });
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.handleSubmit}
+        handleSubmit
+        className="wholeWidth"
+        encType="multipart/form-data"
+      >
+        <FormGroup row>
+          <Col sm={2}>
+            <Input
+              value={this.state.cleanName}
+              type="file"
+              name="excelFile"
+              placeholder="Excel File"
+              onChange={this.handleChange}
+              accept=".xls, .xlsx"
+            />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above
+              input. It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </Col>
+          <Col sm={2} className="pt-3">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle className="bg-gradient-theme-left border-0" caret>
+                Zona Republica
+              </DropdownToggle>
+              <DropdownMenu onChange={this.handleChange} name="zonaRepublica">
+                <DropdownItem>Norte</DropdownItem>
+                <DropdownItem>Bajio</DropdownItem>
+                <DropdownItem>Sureste</DropdownItem>
+                <DropdownItem>Centro</DropdownItem>
+                <DropdownItem>Pacifico</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Col sm={10}>
+            <Button
+              type="submit"
+              size="lg"
+              className="bg-gradient-theme-left border-0 centerButton"
+              block
+              onClick={this.handleSubmit}
+            >
+              Subir Excel
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    );
+  }
 }
 
 export default UploadExcelForm;
--- src/pages/tiros/UploadExcel.js
+++ src/pages/tiros/UploadExcel.js
@@ -1,33 +1,31 @@
-import Page from '../../components/Page';
-import React, {Component} from 'react';
-import {Card, CardBody, CardHeader, Col, Row} from 'reactstrap';
+import Page from "../../components/Page";
+import React, { Component } from "react";
+import { Card, CardBody, CardHeader, Col, Row } from "reactstrap";
 import UploadExcelForm from "../../components/Forms/UploadExcelForm";
 
 class UploadExcel extends Component {
-
-    render() {
-        return (
-            <Page
-                title=""
-                breadcrumbs={[{name: 'tiros/upload/excel', active: true}]}
-                className="TirosUplaodExcelPage"
-            >
-
+  render() {
+    return (
+      <Page
+        title=""
+        breadcrumbs={[{ name: "tiros/upload/excel", active: true }]}
+        className="TirosUplaodExcelPage"
+      >
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>Subir Excel Con Los Tiros</CardHeader>
+              <CardBody>
                 <Row>
-                    <Col>
-                        <Card className="mb-3">
-                            <CardHeader>Subir Excel Con Los Tiros</CardHeader>
-                            <CardBody>
-                                <Row>
-                                    <UploadExcelForm/>
-                                </Row>
-                            </CardBody>
-                        </Card>
-                    </Col>
+                  <UploadExcelForm />
                 </Row>
-            </Page>
-        );
-    }
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Page>
+    );
+  }
 }
 
 export default UploadExcel;
